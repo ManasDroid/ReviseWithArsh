f263c626c03f08fe76f2ff60c21ce302511e17fd
Manas Ranjan Patra <53658835+ManasDroid@users.noreply.github.com>
2023-01-28 23:54:47 +0530

    first commit

---
+++ Microsoft/ReversePolishNotation.java
@@ -0,0 +1,30 @@
+package Microsoft;
+
+import java.util.Stack;
+
+public class ReversePolishNotation {
+    class Solution {
+        public int evalRPN(String[] tokens) {
+            Stack<Integer> st = new Stack<>();
+            for(String t : tokens){
+                if("+-*/".contains(t))
+                    st.push(eval(st.pop(),st.pop(),t));
+                else
+                    st.push(Integer.parseInt(t));
+            }
+
+            return st.pop();
+        }
+
+        private int eval(int b, int a, String op){
+            if("+".equals(op))
+                return a+b;
+            else if("-".equals(op))
+                return a-b;
+            else if("*".equals(op))
+                return a*b;
+            else
+                return a/b;
+        }
+    }
+}
